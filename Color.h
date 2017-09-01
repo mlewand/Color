95bb8b7d9c57fc7988eefc41e4cd3cb4dcd3e19f
Marek Lewandowski <marek@mlewandowski.com>
2017-09-02 00:18:57 +0200

    Code style.

--- Color.h
+++ Color.h
@@ -5,32 +5,37 @@
 typedef byte color_t;
 typedef uint32_t long_color_t;
 
-class Color
-{
+class Color {
 private:
 	color_t red;
 	color_t green;
 	color_t blue;
 
 public:
-	Color(color_t, color_t, color_t);
-	Color(long_color_t);
+	Color( color_t, color_t, color_t );
+	Color( long_color_t );
 
 	// Darkens current color (all channels) by a given literal.
-	Color *darken(color_t);
+	Color *darken( color_t );
 
 	// Lightens current color (all channels) by a given literal.
-	Color *lighten(color_t);
+	Color *lighten( color_t );
 
 	// percentage - 0-100 value
-	Color *blend(const Color *other, byte percentage);
+	Color *blend( const Color *other, byte percentage );
 
-	color_t getRed() const { return this->red; }
-	color_t getGreen() const { return this->green; }
-	color_t getBlue() const { return this->blue; }
+	color_t getRed() const {
+		return this->red;
+	}
+	color_t getGreen() const {
+		return this->green;
+	}
+	color_t getBlue() const {
+		return this->blue;
+	}
 
 	operator long_color_t() const;
-	Color &operator=(const Color &otherColor);
+	Color &operator=( const Color &otherColor );
 };
 
 #endif
