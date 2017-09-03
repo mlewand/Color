2d45715ab4902f05a44eec17b2a4a2c4469e4fa1
Marek Lewandowski <marek@mlewandowski.com>
2017-09-03 14:10:48 +0200

    Unified formatting.

--- Color.cpp
+++ Color.cpp
@@ -5,27 +5,31 @@
 // ESP8266 driver defines it's own max implementation, which prevents me from using pure stdlib implementation.
 // https://github.com/esp8266/Arduino/issues/398
 #include <stdlib.h>
-#define _color_max( a, b ) std::max( ( color_t )a, ( color_t )b )
-#define _color_min( a, b ) std::min( ( color_t )a, ( color_t )b )
+#define _color_max(a, b) std::max((color_t)a, (color_t)b)
+#define _color_min(a, b) std::min((color_t)a, (color_t)b)
 #else
-#define _color_max( a, b ) max( a, b )
-#define _color_min( a, b ) min( a, b )
+#define _color_max(a, b) max(a, b)
+#define _color_min(a, b) min(a, b)
 #endif
 
-Color::Color( color_t red, color_t green, color_t blue ) {
+Color::Color(color_t red, color_t green, color_t blue)
+{
 	this->red = red;
 	this->green = green;
 	this->blue = blue;
 }
 
-Color::Color( long_color_t rgbColor ) {
+Color::Color(long_color_t rgbColor)
+{
 	this->red = rgbColor >> 16;
-	this->green = ( rgbColor >> 8 ) & 255;
+	this->green = (rgbColor >> 8) & 255;
 	this->blue = rgbColor & 255;
 }
 
-Color &Color::operator=( const Color &otherColor ) {
-	if ( this != &otherColor ) {
+Color &Color::operator=(const Color &otherColor)
+{
+	if (this != &otherColor)
+	{
 		this->red = otherColor.red;
 		this->green = otherColor.green;
 		this->blue = otherColor.blue;
@@ -34,35 +38,39 @@ Color &Color::operator=( const Color &otherColor ) {
 	return *this;
 }
 
-Color *Color::darken( color_t darkenLiteral ) {
-	this->red = ( red > darkenLiteral ) ? red - darkenLiteral : 0;
-	this->green = ( green > darkenLiteral ) ? green - darkenLiteral : 0;
-	this->blue = ( blue > darkenLiteral ) ? blue - darkenLiteral : 0;
+Color *Color::darken(color_t darkenLiteral)
+{
+	this->red = (red > darkenLiteral) ? red - darkenLiteral : 0;
+	this->green = (green > darkenLiteral) ? green - darkenLiteral : 0;
+	this->blue = (blue > darkenLiteral) ? blue - darkenLiteral : 0;
 
 	return this;
 }
 
-Color *Color::lighten( color_t lightenLiteral ) {
-	this->red = ( red + lightenLiteral <= 255 ) ? red + lightenLiteral : 255;
-	this->green = ( green + lightenLiteral <= 255 ) ? green + lightenLiteral : 255;
-	this->blue = ( blue + lightenLiteral <= 255 ) ? blue + lightenLiteral : 255;
+Color *Color::lighten(color_t lightenLiteral)
+{
+	this->red = (red + lightenLiteral <= 255) ? red + lightenLiteral : 255;
+	this->green = (green + lightenLiteral <= 255) ? green + lightenLiteral : 255;
+	this->blue = (blue + lightenLiteral <= 255) ? blue + lightenLiteral : 255;
 
 	return this;
 }
 
-Color *Color::blend( const Color *other, byte percentage ) {
-	percentage = _color_min( percentage, 100 );
-	percentage = _color_max( percentage, 0 );
+Color *Color::blend(const Color *other, byte percentage)
+{
+	percentage = _color_min(percentage, 100);
+	percentage = _color_max(percentage, 0);
 
-	this->red = ( ( double )other->red * percentage / 100 ) + ( ( double )this->red * ( 100 - percentage ) / 100 );
-	this->green = ( ( double )other->green * percentage / 100 ) + ( ( double )this->green * ( 100 - percentage ) / 100 );
-	this->blue = ( ( double )other->blue * percentage / 100 ) + ( ( double )this->blue * ( 100 - percentage ) / 100 );
+	this->red = ((double)other->red * percentage / 100) + ((double)this->red * (100 - percentage) / 100);
+	this->green = ((double)other->green * percentage / 100) + ((double)this->green * (100 - percentage) / 100);
+	this->blue = ((double)other->blue * percentage / 100) + ((double)this->blue * (100 - percentage) / 100);
 
 	return this;
 }
 
-Color::operator long_color_t() const {
-	return ( ( long_color_t )this->red << 16 ) | ( ( long_color_t )this->green << 8 ) | this->blue;
+Color::operator long_color_t() const
+{
+	return ((long_color_t)this->red << 16) | ((long_color_t)this->green << 8) | this->blue;
 }
 
 // Do not leave preprocessor garbage.
--- Color.h
+++ Color.h
@@ -5,37 +5,41 @@
 typedef byte color_t;
 typedef uint32_t long_color_t;
 
-class Color {
-private:
+class Color
+{
+  private:
 	color_t red;
 	color_t green;
 	color_t blue;
 
-public:
-	Color( color_t, color_t, color_t );
-	Color( long_color_t );
+  public:
+	Color(color_t, color_t, color_t);
+	Color(long_color_t);
 
 	// Darkens current color (all channels) by a given literal.
-	Color *darken( color_t );
+	Color *darken(color_t);
 
 	// Lightens current color (all channels) by a given literal.
-	Color *lighten( color_t );
+	Color *lighten(color_t);
 
 	// percentage - 0-100 value - percentage of current color to be left in blend color.
-	Color *blend( const Color *other, byte percentage );
+	Color *blend(const Color *other, byte percentage);
 
-	color_t getRed() const {
+	color_t getRed() const
+	{
 		return this->red;
 	}
-	color_t getGreen() const {
+	color_t getGreen() const
+	{
 		return this->green;
 	}
-	color_t getBlue() const {
+	color_t getBlue() const
+	{
 		return this->blue;
 	}
 
 	operator long_color_t() const;
-	Color &operator=( const Color &otherColor );
+	Color &operator=(const Color &otherColor);
 };
 
 #endif
