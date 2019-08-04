52d737193829ccaab6d85a66ca80b6652230626d
Marek Lewandowski <marek@mlewandowski.com>
2019-08-04 16:12:53 +0200

    Added a workaround for FastLED conflict caused by PlatformIO.

--- include/Color/Color.h
+++ include/Color/Color.h
@@ -1,1 +1,16 @@
 #include "./../Color.h"
+
+/*
+
+--------------------------------------------------------------------
+#
+# This file exists only due to a sad fact that FastLED has a Color.h file which might
+# take a precedence over this one.
+#
+# Since PlatformIO doesn't care about library scoping, it's uncertain which file will be loaded.
+# See https://community.platformio.org/t/handling-dependencies-that-use-the-same-header-file-names/9037
+#
+#
+--------------------------------------------------------------------
+
+*/
